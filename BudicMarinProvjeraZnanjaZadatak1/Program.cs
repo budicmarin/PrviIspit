461ddfc0f48cbbc0d05b7939d6bbb30497222a33
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-02-13 09:49:32 +0100

    Prvi Zadatak

---
+++ BudicMarinProvjeraZnanjaZadatak1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudicMarinProvjeraZnanjaZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 7;
+            double c = 12.5;
+            double d = 16.7;
+            double prosjek = a + b + c + d / (double)4;
+            Console.WriteLine(prosjek);
+            Console.ReadKey();
+        }
+    }
+}
