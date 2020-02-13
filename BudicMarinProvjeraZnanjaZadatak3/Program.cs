55539e3c02e0afc0c30fba7ad6daa715ef0bd20f
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-02-13 10:59:20 +0100

    Zadatak 3

---
+++ BudicMarinProvjeraZnanjaZadatak3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudicMarinProvjeraZnanjaZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisi rijec ");
+            string rijec = Console.ReadLine();
+
+            string temp = " ";
+            for (int i = rijec.Length-1; i >= 0; i--)
+            {
+
+
+                if (i == 0)
+                {
+                    Console.WriteLine(rijec.Substring(i));
+                }
+                else
+                {
+                    
+                    Console.WriteLine(temp.PadRight(i)+rijec.Substring(i));
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
